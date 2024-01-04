e1b4bf11a819b919c558a80e55e1d37368dae85a
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-04 11:18:58 +0700

    create js untuk sidebar dan navbar

---
+++ static/theme/bar/sidebar.js
@@ -0,0 +1,5 @@
+// Untuk Render Sidebar
+document.addEventListener('DOMContentLoaded', function() {
+    $("#sidebar").load("https://pmb.ulbi.ac.id/pmb-mhs/static/sidebar.html");
+    console.log('DOM fully loaded and parsed');
+  });
