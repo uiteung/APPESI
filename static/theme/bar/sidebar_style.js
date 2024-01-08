ffc209ce40d11ed9ab7a79ce8e69c80fb356a01c
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-08 13:47:27 +0700

    create sidebar_style.js untuk persiapan sidebar

---
+++ static/theme/bar/sidebar_style.js
@@ -0,0 +1,22 @@
+// import {  } from "../../js/controller/template.js";
+// import { token } from "../../js/controller/cookies.js";
+// import { setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
+// import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
+
+
+// Pengkondisian Badge Pada Biodata
+// Data Jalur Pendaftaran
+// getWithHeader(UrlGetBiodataJalurWithToken, "LOGIN", token, responseDataJalur)
+
+// function responseDataJalur(value) {
+//   const bgWarning = 'badge bg-warning-500 absolute text-white capitalize top-0 right-0 mt-1 mr-1';
+//   const bgSuccess = 'badge bg-success-500 absolute text-white capitalize top-0 right-0 mt-1 mr-1';
+//   if (value.success) {
+//     setInner('badgeJalur', 'Sudah Diisi');
+//     // Show the hidden button 
+//     document.getElementById("badgeJalur").className = bgSuccess;
+//   } else {
+//     setInner('badgeJalur', 'Belum Diisi');
+//     document.getElementById("badgeJalur").className = bgWarning;
+//   }
+// }
