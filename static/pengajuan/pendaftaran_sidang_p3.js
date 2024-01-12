59fb730815a88ebb1319652555c5e4a69081e765
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-12 14:43:31 +0700

    create sementara function untuk pendaftaran sidang p3

--- static/pengajuan/pendaftaran_sidang_p3.js
+++ static/pengajuan/pendaftaran_sidang_p3.js
@@ -6,7 +6,13 @@ import { token } from "../controller/cookies.js";
 postWithToken(UrlCekPendaftaranSidangP3, "LOGIN", token, "test", ResponseSidangP3)
 
 function ResponseSidangP3(value) {
+    const bgSuccess = 'alert alert-success';
+
     if (value.data) {
-        setInner('AlertDaftarSidang')
+        setInner('AlertDaftarSidang', 'Sebelum melakukan pendaftaran sidang, silahkan cek nilai matakuliah prasyaratnya <a href="">di sini</a>!');
+        document.getElementById('AlertDaftarSidang').className = bgSuccess;
+        console.log("Masuk ke Success value");
+    } else {
+        console.log(value);
     }
 }
