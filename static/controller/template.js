98619fb02dfaa3e506fedcac56dcd8dee07e5cd8
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-15 10:56:36 +0700

    add endpoint url cek role

--- static/controller/template.js
+++ static/controller/template.js
@@ -1,2 +1,4 @@
+// Import Url
 export let UrlCekPendaftaranSidangP3 = "";
 export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
+export let UrlCekRole = "https://p3-be.ulbi.ac.id/api/sidang/dosen";
