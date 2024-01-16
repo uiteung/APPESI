ebec77c72239743d0e02ea57fdf47b9c9db85f4b
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-16 15:09:32 +0700

    add UrlGetAllPersyaratan

--- static/controller/template.js
+++ static/controller/template.js
@@ -2,3 +2,4 @@
 export let UrlCekPendaftaranSidangP3 = "";
 export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
 export let UrlCekRole = "https://p3-be.ulbi.ac.id/api/sidang/dosen";
+export let UrlGetAllPersyaratan = "https://p3-be.ulbi.ac.id/api/sidang/syarat/all";
