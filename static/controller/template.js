0721dd24183a80a51a666a4bdddd64fdb8636dc1
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-16 15:09:40 +0700

    rapihin endpoint di template

--- static/controller/template.js
+++ static/controller/template.js
@@ -1,5 +1,7 @@
-// Import Url
+// Endpoint GET
 export let UrlCekPendaftaranSidangP3 = "";
-export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
 export let UrlCekRole = "https://p3-be.ulbi.ac.id/api/sidang/dosen";
 export let UrlGetAllPersyaratan = "https://p3-be.ulbi.ac.id/api/sidang/syarat/all";
+
+// Endpoint POST
+export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
