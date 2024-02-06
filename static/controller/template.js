fb27c099a670496ba848a8f49e907d62008fcef9
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-06 21:07:01 +0700

    add UrlGetNilaiByToken

--- static/controller/template.js
+++ static/controller/template.js
@@ -6,6 +6,7 @@ export let UrlGetAllNilaiP3 = "https://kimteungbim.ulbi.ac.id/sidang/p3/nilai";
 
 // Endpoint GET By ID
 export let UrlGetNilaiP3ByNPM = "https://kimteungbim.ulbi.ac.id/sidang/p3/npmnilai";
+export let UrlGetNilaiP3ByToken = "https://kimteungbim.ulbi.ac.id/sidang/p3/mhsnilai"
 
 // Endpoint POST
 export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
