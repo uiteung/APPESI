512d9769f860bbc6a6c2c6e385b42d37bd0eb540
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-06 20:31:34 +0700

    create codeToNameMapping

--- static/cek_nilai.js
+++ static/cek_nilai.js
@@ -6,6 +6,21 @@ import { token } from "./controller/cookies.js"
 
 // Untuk Cek Nilai Sidang Mahasiswa
 document.addEventListener('DOMContentLoaded', function () {
+    // Mapping NIDN ke Nama
+    const codeToNameMapping = {
+        "0420058801" : "Roni Andarsyah, S.T.,M.Kom.,SFPC",
+        "0427078401" : "Cahyo Prianto, S.Pd.,M.T.,CDSP.,SFPC",
+        "0407117405" : "M. Yusril Helmi Setyawan, S.Kom.,M.Kom.,SFPC",
+        "0410118609" : "Rolly Maulana Awangga, S.T.,MT.,CAIP,SFPC",
+        "0402058005" : "Mohamad Nurkamal Fauzan, S.T.,M.T.,SFPC",
+        "0423127804" : "Roni Habibi, S.Kom.,M.T.,SFPC",
+        "0416048803" : "Syafrial Fachri Pane,ST. M.TI.,EBDP.,CDSP.,SFPC",
+        "0402047205" : "Rd. Nuraini Siti Fatonah, S.S.,M.Hum.,SFPC",
+        "0415048901" : "Nisa Hanum Harani, S.Kom.,M.T.,CDSP.,SFPC",
+        "0415107901" : "Woro Isti Rahayu, S.T.,M.T.,SFPC",
+        "0403117607" : "Noviana Riza, S.Si.,M.T.,SFPC",
+    };
+    
     const btnCekNilai = document.querySelector('#btnCekNilai');
     const inputNPM = document.querySelector('#inputNPM');
     
