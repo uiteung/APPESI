2430300d3c08b4ea24fba336124622aa717928b6
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-20 16:10:28 +0700

    import function, url, and token

---
+++ static/biodata/profil.js
@@ -0,0 +1,3 @@
+import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
+import { UrlGetBiodataByToken } from "../controller/template.js";
+import { token } from "../controller/cookies.js";
