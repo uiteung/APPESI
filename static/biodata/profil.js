5b135c388bb097cb74cbab300d8ee5c2156604fd
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-20 16:20:53 +0700

    add set value from jscroot

--- static/biodata/profil.js
+++ static/biodata/profil.js
@@ -1,4 +1,5 @@
 import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
+import { setValue } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
 import { UrlGetBiodataByToken } from "../controller/template.js";
 import { token } from "../controller/cookies.js";
 
