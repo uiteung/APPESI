1a953e16c4c1a87d71d7face718d701907cb88e2
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-23 11:15:20 +0700

    create style for form pendaftaran

---
+++ static/pendaftaran/style_daftar.js
@@ -0,0 +1,29 @@
+// Membuat variabel untuk menyimpan elemen judul
+var titleElement = document.getElementById("titleDaftar");
+
+// Menambahkan event listener ke dropdown untuk mendengarkan perubahan
+document.getElementById("selectTipeBimbingan").addEventListener("change", function() {
+    // Mendapatkan nilai terpilih dari dropdown
+    var selectedValue = this.value;
+    
+    // Membuat objek map untuk menghubungkan nilai dropdown dengan judul yang sesuai
+    var titleMap = {
+      "p1": "Proyek 1",
+      "p2": "Proyek 2",
+      "p3": "Proyek 3",
+      "i1": "Internship 1",
+      "i2": "Internship 2",
+      "ta": "Tugas Akhir"
+    };
+  
+    // Mengubah teks judul sesuai dengan nilai dropdown yang dipilih
+    titleElement.innerText = "Pendaftaran " + titleMap[selectedValue];
+  
+    // Jika opsi yang dipilih adalah "Tugas Akhir", tampilkan input pembimbing 2
+    if (selectedValue === "ta") {
+      document.getElementById("selectPembimbing2").parentNode.removeAttribute("hidden");
+    } else {
+      // Sembunyikan input pembimbing 2 jika opsi yang dipilih bukan "Tugas Akhir"
+      document.getElementById("selectPembimbing2").parentNode.setAttribute("hidden", "true");
+    }
+});
