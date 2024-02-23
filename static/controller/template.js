4be9607549ea5f3c3adcf72a49ddc33e33accea2
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-23 11:29:08 +0700

    add new endpoint in template

--- static/controller/template.js
+++ static/controller/template.js
@@ -8,6 +8,8 @@ export let UrlGetAllNilaiP3 = "https://kimteungbim.ulbi.ac.id/sidang/p3/nilai";
 export let UrlGetNilaiP3ByNPM = "https://kimteungbim.ulbi.ac.id/sidang/p3/npmnilai";
 export let UrlGetNilaiP3ByToken = "https://kimteungbim.ulbi.ac.id/sidang/p3/mhsnilai"
 export let UrlGetBiodataByToken = "https://kimteungbim.ulbi.ac.id/p1/mhs/bio";
+export let UrlGetCountMhs = "https://kimteungbim.ulbi.ac.id/p1/mhs/count";
+export let UrlGetAllPengajuanByNIDN = "https://kimteungbim.ulbi.ac.id/p1/dosen/pengajuan";
 
 // Endpoint POST
 export let UrlPostPendaftaranSidangP3 = "https://p3-be.ulbi.ac.id/api/sidang/syarat";
